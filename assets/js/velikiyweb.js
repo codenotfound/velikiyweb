939dfef223f8866428f162956771da0b92f4b7dc
codenotfound <dmitry.velikij@gmail.com>
2016-01-11 23:14:11 +0300

    tested and added strip.js

--- assets/js/velikiyweb.js
+++ assets/js/velikiyweb.js
@@ -1,2 +1,10 @@
 $(document).ready(function() {
+  $('#iClick').on('click', function(event) {
+    // the page will scroll up without this
+    event.preventDefault();
+    Strip.show([
+      { url: 'http://placehold.it/700x701', caption: 'Caption below the image' },
+      { url: 'http://placehold.it/700x702', caption: 'Another caption' }
+    ]);
+  });
 });

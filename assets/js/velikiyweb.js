89686a915d0be44f3c4ccc5371b84b66db1fcdf4
codenotfound <dmitry.velikij@gmail.com>
2016-01-12 15:19:12 +0300

    too glorious not to save

--- assets/js/velikiyweb.js
+++ assets/js/velikiyweb.js
@@ -3,8 +3,26 @@ $(document).ready(function() {
     // the page will scroll up without this
     event.preventDefault();
     Strip.show([
-      { url: 'http://placehold.it/700x701', caption: 'Caption below the image' },
-      { url: 'http://placehold.it/700x702', caption: 'Another caption' }
+      { url: 'assets/img/tryfoto_1080p.png', 
+          caption: 'Адаптивная верстка для проекта tryfoto.ru' },
+      { url: 'assets/img/tryfoto_tablet.png', 
+          caption: 'Адаптивная верстка для проекта tryfoto.ru' },
+      { url: 'assets/img/tryfoto_iphone.png', 
+          caption: 'Адаптивная верстка для проекта tryfoto.ru' },
+      { url: 'assets/img/tryfoto_nexus.png', 
+          caption: 'Адаптивная верстка для проекта tryfoto.ru' },
+      { url: 'https://media.giphy.com/media/xT77XKxcPqxIZqUrwk/giphy.gif', 
+          caption: 'Адаптивная верстка для проекта tryfoto.ru' },
+    ]);
+  });
+  $('#grid_gk_br').on('click', function(event) {
+    // the page will scroll up without this
+    event.preventDefault();
+    Strip.show([
+      { url: 'assets/img/tryfoto_1080p.png', 
+          caption: 'Адаптивная верстка сайта Группы Компаний "Бизнес-решения"' },
+      { url: 'assets/img/gk_br_interactive.gif', 
+          caption: 'Интерактивные элементы' },
     ]);
   });
 });
